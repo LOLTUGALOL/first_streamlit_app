8ae6a8f22970ff55c4a27745e0d69b4a2d87705d
Goncalo Mena <76131699+LOLTUGALOL@users.noreply.github.com>
2023-04-03 11:54:13 +0100

    Added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title('My first app in streamlit')
-
+streamlit.header('Just some casual header.')
 streamlit.text('This is my first app in streamlit done in the workshop by Snowflake.')
